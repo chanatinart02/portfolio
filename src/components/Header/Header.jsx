dfba266ac5e1c32ec581c7236442f71db95963fd
Chanatinart K <125489141+chanatinart02@users.noreply.github.com>
2023-05-14 00:14:07 +0700

    setup project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,82 @@
+import "./Header.css";
+
+import { NavLink } from "react-router-dom";
+
+import {
+  AiOutlineHome,
+  AiOutlineFundProjectionScreen,
+  AiOutlineUser,
+} from "react-icons/ai";
+
+function Header() {
+  const toggleShow = () => {
+    document.getElementById("expand").setAttribute("aria-expanded", false);
+    document.getElementById("navbarNav").classList.toggle("show");
+  };
+  return (
+    <header className="sticky-top">
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <div className="container-fluid">
+            <a className="navbar-brand w-25" href="#">
+              <img className="w-75" src="icons/logo.png" alt="" />
+            </a>
+            <button
+              id="expand"
+              className="navbar-toggler bg-white"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon bg-white"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse justify-content-end"
+              id="navbarNav"
+            >
+              <ul onClick={toggleShow} className="navbar-nav my-nav-style">
+                <li className="nav-item ms-1">
+                  <NavLink
+                    to="/"
+                    exact={true}
+                    className="nav-link text-white"
+                    activeClassName="active"
+                  >
+                    <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+                  </NavLink>
+                </li>
+                <li className="nav-item ms-1">
+                  <NavLink
+                    to="/about"
+                    className="nav-link text-white"
+                    activeClassName="active"
+                  >
+                    <AiOutlineUser style={{ marginBottom: "2px" }} /> About Me
+                  </NavLink>
+                </li>
+
+                <li className="nav-item ms-1">
+                  <NavLink
+                    to="/projects"
+                    className="nav-link text-white"
+                    activeClassName="active"
+                  >
+                    <AiOutlineFundProjectionScreen
+                      style={{ marginBottom: "2px" }}
+                    />
+                    Project
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
